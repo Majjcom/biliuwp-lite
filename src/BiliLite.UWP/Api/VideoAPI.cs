40c0dd7ab55edf0f69c1ff5eca8e7997b0ab69d2
Majjcom <majjcom@126.com>
2023-08-29 22:54:53 +0800

    update video api

--- src/BiliLite.UWP/Api/VideoAPI.cs
+++ src/BiliLite.UWP/Api/VideoAPI.cs
@@ -10,27 +10,41 @@ namespace BiliLite.Api
     {
         public ApiModel Detail(string id,bool isbvid)
         {
+            //ApiHelper.MustParameter(ApiHelper.AndroidKey, true) + $"&{(isbvid?"bvid=":"aid=")}{id}&plat=0"
             ApiModel api = new ApiModel()
             {
                 method = RestSharp.Method.Get,
                 baseUrl = $"https://app.bilibili.com/x/v2/view",
-                parameter = ApiHelper.MustParameter(ApiHelper.AndroidKey, true) + $"&{(isbvid?"bvid=":"aid=")}{id}&plat=0"
+                parameter = new ApiParameter
+                {
+                    { isbvid ? "bvid" : "aid", id },
+                    { "plat", "0" }
+                } + ApiHelper.MustParameter(ApiHelper.AndroidKey, true)
             };
             api.parameter += ApiHelper.GetSign(api.parameter, ApiHelper.AndroidKey);
             return api;
         }
         public ApiModel DetailProxy(string id, bool isbvid)
         {
+            //ApiHelper.MustParameter(ApiHelper.AndroidKey, true) + $"&{(isbvid ? "bvid=" : "aid=")}{id}&plat=0"
             ApiModel api = new ApiModel()
             {
                 method = RestSharp.Method.Get,
                 baseUrl = $"https://app.bilibili.com/x/v2/view",
-                parameter = ApiHelper.MustParameter(ApiHelper.AndroidKey, true) + $"&{(isbvid ? "bvid=" : "aid=")}{id}&plat=0"
+                parameter = new ApiParameter
+                {
+                    { isbvid ? "bvid" : "aid", id },
+                    { "plat", "0" }
+                } + ApiHelper.MustParameter(ApiHelper.AndroidKey, true)
             };
             api.parameter += ApiHelper.GetSign(api.parameter, ApiHelper.AndroidKey);
             var apiUrl = Uri.EscapeDataString(api.url);
             api.baseUrl = "https://biliproxy.iill.moe/app.php";
-            api.parameter = "url=" + apiUrl;
+            //"url=" + apiUrl;
+            api.parameter = new ApiParameter
+            {
+                { "url", apiUrl }
+            };
             return api;
         }
         /// <summary>
@@ -41,13 +55,22 @@ namespace BiliLite.Api
         /// <returns></returns>
         public ApiModel Like(string aid, int dislike,int like)
         {
+            //ApiHelper.MustParameter(ApiHelper.AndroidKey, true) + $"&aid={aid}&dislike={dislike}&from=7&like={like}"
+            ApiParameter body = new ApiParameter
+            {
+                { "aid", aid },
+                { "dislike", dislike.ToString() },
+                { "from", "7" },
+                { "like", like.ToString() }
+            } + ApiHelper.MustParameter(ApiHelper.AndroidKey, true);
+
             ApiModel api = new ApiModel()
             {
                 method = RestSharp.Method.Post,
                 baseUrl = $"https://app.bilibili.com/x/v2/view/like",
-                body = ApiHelper.MustParameter(ApiHelper.AndroidKey, true) + $"&aid={aid}&dislike={dislike}&from=7&like={like}"
+                body = body.ToString()
             };
-            api.body += ApiHelper.GetSign(api.body, ApiHelper.AndroidKey);
+            api.body += '&' + ApiHelper.GetSign(api.body, ApiHelper.AndroidKey).ToString();
             return api;
         }
         /// <summary>
@@ -58,41 +81,64 @@ namespace BiliLite.Api
         /// <returns></returns>
         public ApiModel Dislike(string aid, int dislike, int like)
         {
+            //ApiHelper.MustParameter(ApiHelper.AndroidKey, true) + $"&aid={aid}&dislike={dislike}&from=7&like={like}"
+            ApiParameter body = new ApiParameter
+            {
+                { "aid", aid },
+                { "dislike", dislike.ToString() },
+                { "from", "7" },
+                { "like", like.ToString() }
+            } + ApiHelper.MustParameter(ApiHelper.AndroidKey, true);
+
             ApiModel api = new ApiModel()
             {
                 method = RestSharp.Method.Post,
                 baseUrl = $"https://app.biliapi.net/x/v2/view/dislike",
-                body = ApiHelper.MustParameter(ApiHelper.AndroidKey, true) + $"&aid={aid}&dislike={dislike}&from=7&like={like}"
+                body = body.ToString()
             };
-            api.body+= ApiHelper.GetSign(api.body, ApiHelper.AndroidKey);
+            api.body += '&' + ApiHelper.GetSign(api.body, ApiHelper.AndroidKey).ToString();
             return api;
         }
 
         /// <summary>
-        ///一键三连
+        /// 一键三连
         /// </summary>
         /// <returns></returns>
         public ApiModel Triple(string aid)
         {
+            //ApiHelper.MustParameter(ApiHelper.AndroidKey, true) + $"&aid={aid}"
+            ApiParameter body = new ApiParameter
+            {
+                { "aid", aid }
+            } + ApiHelper.MustParameter(ApiHelper.AndroidKey, true);
+
             ApiModel api = new ApiModel()
             {
                 method = RestSharp.Method.Post,
                 baseUrl = $"https://app.bilibili.com/x/v2/view/like/triple",
-                body = ApiHelper.MustParameter(ApiHelper.AndroidKey, true) + $"&aid={aid}"
+                body = body.ToString()
             };
-            api.body += ApiHelper.GetSign(api.body, ApiHelper.AndroidKey);
+            api.body += '&' + ApiHelper.GetSign(api.body, ApiHelper.AndroidKey).ToString();
             return api;
         }
 
         public ApiModel Coin(string aid, int num)
         {
+            //ApiHelper.MustParameter(ApiHelper.AndroidKey, true) + $"&aid={aid}&multiply={num}&platform=android&select_like=0"
+            ApiParameter body = new ApiParameter
+            {
+                { "aid", aid },
+                { "multiply", num.ToString() },
+                { "select_like", "0" }
+            } + ApiHelper.MustParameter(ApiHelper.AndroidKey, true);
+
             ApiModel api = new ApiModel()
             {
                 method = RestSharp.Method.Post,
                 baseUrl = $"https://app.biliapi.net/x/v2/view/coin/add",
-                body = ApiHelper.MustParameter(ApiHelper.AndroidKey, true) + $"&aid={aid}&multiply={num}&platform=android&select_like=0"
+                body = body.ToString()
             };
-            api.body += ApiHelper.GetSign(api.body, ApiHelper.AndroidKey);
+            api.body += '&' + ApiHelper.GetSign(api.body, ApiHelper.AndroidKey).ToString();
             return api;
         }
         /// <summary>
@@ -101,18 +147,24 @@ namespace BiliLite.Api
         /// <param name="mid">用户ID</param>
         /// <param name="mode">1为关注，2为取消关注</param>
         /// <returns></returns>
-        public ApiModel Attention(string mid,string mode)
+        public ApiModel Attention(string mid, string mode)
         {
+            //ApiHelper.MustParameter(ApiHelper.AndroidKey, true) + $"&act={mode}&fid={mid}&re_src=32"
+            ApiParameter body = new ApiParameter
+            {
+                { "act", mode },
+                { "fid", mid },
+                { "re_src", "32" }
+            } + ApiHelper.MustParameter(ApiHelper.AndroidKey, true);
+
             ApiModel api = new ApiModel()
             {
                 method = RestSharp.Method.Post,
                 baseUrl = $"{ApiHelper.API_BASE_URL}/x/relation/modify",
-                body = ApiHelper.MustParameter(ApiHelper.AndroidKey, true) + $"&act={mode}&fid={mid}&re_src=32"
+                body = body.ToString()
             };
-            api.body += ApiHelper.GetSign(api.body, ApiHelper.AndroidKey);
+            api.body += '&' + ApiHelper.GetSign(api.body, ApiHelper.AndroidKey).ToString();
             return api;
         }
-
-
     }
 }
