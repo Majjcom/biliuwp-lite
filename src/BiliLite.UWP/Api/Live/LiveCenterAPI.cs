49a1f774b09c7f9741650f21260646b10a2027cd
Majjcom <majjcom@126.com>
2023-08-29 23:30:43 +0800

    update live center api

--- src/BiliLite.UWP/Api/Live/LiveCenterAPI.cs
+++ src/BiliLite.UWP/Api/Live/LiveCenterAPI.cs
@@ -10,55 +10,79 @@ namespace BiliLite.Api.Live
     {
         public ApiModel FollowLive()
         {
+            //ApiHelper.MustParameter(ApiHelper.AndroidKey, true) + "&qn=0&sortRule=0&filterRule=0",
             ApiModel api = new ApiModel()
             {
                 method = RestSharp.Method.Get,
                 baseUrl = "https://api.live.bilibili.com/xlive/app-interface/v1/relation/liveAnchor",
-                parameter = ApiHelper.MustParameter(ApiHelper.AndroidKey, true) + "&qn=0&sortRule=0&filterRule=0",
+                parameter = new ApiParameter
+                {
+                    { "qn", "0" },
+                    { "sortRule", "0" },
+                    { "filterRule", "0" }
+                } + ApiHelper.MustParameter(ApiHelper.AndroidKey, true)
             };
             api.parameter += ApiHelper.GetSign(api.parameter, ApiHelper.AndroidKey);
             return api;
         }
         public ApiModel FollowUnLive(int page)
         {
+            //ApiHelper.MustParameter(ApiHelper.AndroidKey, true) + $"&page={page}&pagesize=30",
             ApiModel api = new ApiModel()
             {
                 method = RestSharp.Method.Get,
                 baseUrl = "https://api.live.bilibili.com/xlive/app-interface/v1/relation/unliveAnchor",
-                parameter = ApiHelper.MustParameter(ApiHelper.AndroidKey, true) + $"&page={page}&pagesize=30",
+                parameter = new ApiParameter
+                {
+                    { "page", page.ToString() },
+                    { "pagesize", "30" }
+                } + ApiHelper.MustParameter(ApiHelper.AndroidKey, true)
             };
             api.parameter += ApiHelper.GetSign(api.parameter, ApiHelper.AndroidKey);
             return api;
         }
         public ApiModel History(int page)
         {
+            //ApiHelper.MustParameter(ApiHelper.AndroidKey, true) + $"&pn={page}&ps=20",
             ApiModel api = new ApiModel()
             {
                 method = RestSharp.Method.Get,
                 baseUrl = "https://app.bilibili.com/x/v2/history/liveList",
-                parameter = ApiHelper.MustParameter(ApiHelper.AndroidKey, true) + $"&pn={page}&ps=20",
+                parameter = new ApiParameter
+                {
+                    { "pn", page.ToString() },
+                    { "ps", "20" }
+                } + ApiHelper.MustParameter(ApiHelper.AndroidKey, true)
             };
             api.parameter += ApiHelper.GetSign(api.parameter, ApiHelper.AndroidKey);
             return api;
         }
         public ApiModel SignInfo()
         {
+            //ApiHelper.MustParameter(ApiHelper.AndroidKey, true) + "&actionKey=appkey",
             ApiModel api = new ApiModel()
             {
                 method = RestSharp.Method.Get,
                 baseUrl = "https://api.live.bilibili.com/rc/v2/Sign/getSignInfo",
-                parameter = ApiHelper.MustParameter(ApiHelper.AndroidKey, true) + "&actionKey=appkey",
+                parameter = new ApiParameter
+                {
+                    { "actionKey", "appkey" }
+                } + ApiHelper.MustParameter(ApiHelper.AndroidKey, true)
             };
             api.parameter += ApiHelper.GetSign(api.parameter, ApiHelper.AndroidKey);
             return api;
         }
         public ApiModel DoSign()
         {
+            //ApiHelper.MustParameter(ApiHelper.AndroidKey, true) + "&actionKey=appkey"
             ApiModel api = new ApiModel()
             {
                 method = RestSharp.Method.Get,
                 baseUrl = $"https://api.live.bilibili.com/rc/v1/Sign/doSign",
-                parameter = ApiHelper.MustParameter(ApiHelper.AndroidKey, true) + "&actionKey=appkey"
+                parameter = new ApiParameter
+                {
+                    { "actionKey", "appkey" }
+                } + ApiHelper.MustParameter(ApiHelper.AndroidKey, true)
             };
             api.parameter += ApiHelper.GetSign(api.parameter, ApiHelper.AndroidKey);
             return api;
