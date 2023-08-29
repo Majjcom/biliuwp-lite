276af730750c3279cbf3cc2c7ca49051a372bde1
Majjcom <majjcom@126.com>
2023-08-29 23:26:37 +0800

    update live area api

--- src/BiliLite.UWP/Api/Live/LiveAreaAPI.cs
+++ src/BiliLite.UWP/Api/Live/LiveAreaAPI.cs
@@ -10,22 +10,38 @@ namespace BiliLite.Api.Live
     {
         public ApiModel LiveAreaList()
         {
+            //ApiHelper.MustParameter(ApiHelper.AndroidKey, true) + $"&actionKey=appkey&need_entrance=1&parent_id=0"
             ApiModel api = new ApiModel()
             {
                 method = RestSharp.Method.Get,
                 baseUrl = $"https://api.live.bilibili.com/room/v1/Area/getList",
-                parameter = ApiHelper.MustParameter(ApiHelper.AndroidKey, true) + $"&actionKey=appkey&need_entrance=1&parent_id=0"
+                parameter = new ApiParameter
+                {
+                    { "actionKey", "appkey" },
+                    { "need_entrance", "1" },
+                    { "parent_id", "0" }
+                } + ApiHelper.MustParameter(ApiHelper.AndroidKey, true)
             };
             api.parameter += ApiHelper.GetSign(api.parameter, ApiHelper.AndroidKey);
             return api;
         }
-        public ApiModel LiveAreaRoomList(int area_id=0,int parent_area_id=0, int page = 1, string sort_type = "online")
+        public ApiModel LiveAreaRoomList(int area_id = 0, int parent_area_id = 0, int page = 1, string sort_type = "online")
         {
+            //ApiHelper.MustParameter(ApiHelper.AndroidKey, true) + $"&actionKey=appkey&area_id={area_id}&cate_id=0&parent_area_id={parent_area_id}&page={page}&page_size=36&sort_type={sort_type}"
             ApiModel api = new ApiModel()
             {
                 method = RestSharp.Method.Get,
                 baseUrl = $"https://api.live.bilibili.com/room/v3/Area/getRoomList",
-                parameter = ApiHelper.MustParameter(ApiHelper.AndroidKey, true) + $"&actionKey=appkey&area_id={area_id}&cate_id=0&parent_area_id={parent_area_id}&page={page}&page_size=36&sort_type={sort_type}"
+                parameter = new ApiParameter
+                {
+                    { "actionKey", "appkey" },
+                    { "area_id", area_id.ToString() },
+                    { "cate_id", "0" },
+                    { "parent_area_id", parent_area_id.ToString() },
+                    { "page", page.ToString() },
+                    { "page_size", "36" },
+                    { "sort_type", sort_type }
+                } + ApiHelper.MustParameter(ApiHelper.AndroidKey, true)
             };
             api.parameter += ApiHelper.GetSign(api.parameter, ApiHelper.AndroidKey);
             return api;
